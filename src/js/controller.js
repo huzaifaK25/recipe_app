2a018172b02eb89049ce1bafab33a8e995a5dee9
DESKTOP-BKI77CE\kashi <huzaifakashif202@gmail.com>
2025-03-17 07:37:32 +0500

    implemented pagination functionality on serched recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,14 @@
 import * as model from './model.js';
 import recipeView from '../views/recipeView.js';
 import searchView from '../views/searchView.js';
+import resultsView from '../views/resultsView.js';
+import paginationView from '../views/paginationView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   const recipeContainer = document.querySelector('.recipe');
@@ -25,6 +31,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,14 +40,27 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // render initial pagination
+    paginationView.render(model.state.search);
   } catch (error) {
     console.log(error);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // render new pagination
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandelerRender(controlRecipes);
   searchView.addHandelerSearch(controlSearchResults);
+  paginationView.addHandelerClick(controlPagination);
 };
 
 init();
